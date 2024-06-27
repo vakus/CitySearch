f3a0dc0f83c778aef9c527945e5bb006ba0a1570
Dominik Sysojew-Osinski <sysojewosinskidominik@gmail.com>
2024-06-27 20:25:31 +0100

    Added Basic performance Benchmark

---
+++ CitySearch.PerformanceTest.Service/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using CitySearch;
+using CitySearch.Service.CityNameLoader;
+using CitySearch.Service.CityNameNormaliser;
+using CitySearch.Service.TreeSearchCityFinder;
+
+BenchmarkRunner.Run<Benchmarks>();
+
+[MemoryDiagnoser]
+public class Benchmarks
+{
+    private ICityFinder finder;
+
+    [Params("LA", "LOS", "GDA", "LON", "ZA", "J", "F")]
+    public string CityPrefix { get; set; }
+
+    [GlobalSetup(Target = nameof(BenchmarkRun))]
+    public void Init()
+    {
+        finder = new TreeSearchCityFinder(new CsvCityNameLoader(new UppercaseInvariantCityNameNormaliser()),
+            new UppercaseInvariantCityNameNormaliser());
+    }
+
+    [Benchmark]
+    public void BenchmarkRun()
+    {
+        finder.Search(CityPrefix);
+    }
+}
